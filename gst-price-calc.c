63fb86d697ef8dc767ed280eb459c0d35e1f87ba
DURGESH SINGH <durgesh20336@gmail.com>
2025-09-19 23:23:36 +0530

    Add files via upload
    Mini Project to Calculate the GST on the MRP

---
+++ gst-price-calc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void gst(int price);
+int main(){
+	
+	int price;
+	
+	printf("\nenter the price of the item:");
+	scanf("%d",&price);
+	
+	gst(price);
+	getch();
+	return 0;
+}
+void gst(int price){
+	float g;
+	g=price+(price*0.81);
+	printf("the price after adding 18%% GST :%.2f",g);
+}
